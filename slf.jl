3a36da2893692d47b2b4ede0a5e863886884021f
bradcownden <cowndenb@gmail.com>
2023-06-20 14:47:45 +0100

    Create slf.jl
    Separate the Sturm-Louiville functions from the main code for easier portability

---
+++ slf.jl
@@ -0,0 +1,45 @@
+#=
+#  Module for specifying functions present in the Sturm-Louiville form of
+#  the L operators. Simplification of the ratios of f(x)/w(x) is assumed to
+#  have been already performed (functions will not be scaled by a density)
+=#
+
+__precompile__()
+module slf
+
+export w, p, pp, gamma, gammap, V
+
+    function s(x::Array, i::Integer) # Automatic datatype matching
+        return sqrt(1 - x[i]^2) 
+    end
+
+    function z(x::Array, i::Integer) # Automatic datatype matching
+        return exp(atanh(x[i]))
+    end
+
+    function w(x::Array, i::Integer) # Automatic datatype matching
+        return 1 / (s(x,i) * (3 - x[i] + 2 * s(x,i)))
+    end
+
+    function p(x::Array, i::Integer) # Automatic datatype matching
+        return -(x[i]^2 - 1) / z(x,i)
+    end
+    
+    function pp(x::Array, i::Integer) # Automatic datatype matching
+        return (x[i] - 1) * (1 + 2 * x[i]) / s(x,i)
+    end
+    
+    function gamma(x::Array, i::Integer) # Automatic datatype matching
+        return (1 + z(x,i)) / sqrt(1 + (1 + z(x,i))^2)
+    end
+    
+    function gammap(x::Array, i::Integer) # Automatic datatype matching
+        return - z(x,i) / ((x[i]^2 - 1) * (2 + z(x,i) * (2 + z(x,i)))^(3/2))
+    end
+    
+    function V(x::Array, i::Integer) # Automatic datatype matching
+        return -3 * s(x,i) * (1 - 4 * s(x,i) + x[i] * (34 - 15 * x[i] + 44 * s(x,i))) /
+        (4 * (x[i] - 1) * (1 + x[i] + s(x,i))^2 * (-3 + x[i] - 2 * s(x,i))^2)
+    end
+end
+
